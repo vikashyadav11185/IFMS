d85921714c8ad5be454efdef4b229b33ff01d9fc
vikashyadav11185 <vikashyadav11185@gmail.com>
2025-04-08 03:30:49 +0530

    Delete backend directory

--- backend/src/main/java/com/nucleusteq/ifms/exception/PasswordMismatchException.java
+++
@@ -1,7 +0,0 @@
-package com.nucleusteq.ifms.exception;
-
-public class PasswordMismatchException extends RuntimeException {
-    public PasswordMismatchException(String message) {
-        super(message);
-    }
-}
